73a998d733eb1db8616198c566bac1982edc07ab
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-03-26 15:32:12 -0300

    Fixed msb + Ejercicio 4

--- Guia 1/msbLabo.py
+++ Guia 1/msbLabo.py
@@ -11,7 +11,6 @@ def calcular_maxi_subconjunto(matriz, k, f, c, conjunto):
     # Asi elimino un par de opciones de conjuntos de menos de k elementos
     if(k > total_elementos_restantes + len(conjunto)):
         return []
-    
     proxima_fila = f + 1
     proxima_columna = c
     if(proxima_fila == len(matriz)):
@@ -26,11 +25,56 @@ def calcular_maxi_subconjunto(matriz, k, f, c, conjunto):
     else: 
         return conjunto_no_agrego
     
+max_actual = -10000  
+llamadas_recu = 0  
+    
+def calcular_maxi_subconjunto(matriz, k):
+    conjunto = [-1] * k
+    return msc(matriz, k, conjunto, 0)    
+    
+def msc(matriz, k, conjunto, i):
+    global max_actual
+    global llamadas_recu
+    if(cantidad_vacios(conjunto) == 0):
+        return conjunto
+    if(cantidad_vacios(conjunto) + i > k):
+        return [-1] * k
+    opciones_disponibles = calcular_opciones(conjunto)
+    conjunto_max = conjunto[:]
+    for o in range(len(opciones_disponibles)):
+        if opciones_disponibles[o] == True:
+            agrego = conjunto[:]
+            agrego[i] = o
+            # Poda en el cuerpo
+            suma_parcial_actual = sumar_posiciones(matriz, agrego)
+            if(suma_parcial_actual > max_actual): 
+                llamadas_recu += 1
+                conjunto_agrego = msc(matriz, k, agrego, i+1)
+                if(sumar_posiciones(matriz, conjunto_agrego) > sumar_posiciones(matriz, conjunto_max)): 
+                    conjunto_max = conjunto_agrego
+                    max_actual = sumar_posiciones(matriz, conjunto_agrego)
+    return conjunto_max          
+
+def cantidad_vacios(conjunto):
+    res = 0
+    for i in range(len(conjunto)):
+        if(conjunto[i] == -1):
+            res += 1
+    return res
+
+def calcular_opciones(conjunto):
+    opciones = [True] * len(conjunto)
+    for i in range(len(conjunto)):
+        if(conjunto[i] != -1):
+            opciones[conjunto[i]] = False
+    return opciones
+
 def sumar_posiciones(matriz, conjunto):
     res = 0
     for f in range(len(conjunto)):
         for col in range(len(conjunto)):
-            res += matriz[conjunto[f]][conjunto[col]]
+            if(conjunto[f] != -1 and conjunto[col] != -1):
+                res += matriz[conjunto[f]][conjunto[col]]
     return res
 
 matriz = [
@@ -40,4 +84,4 @@ matriz = [
     [1,2,1,0]
 ]
 
-print(calcular_maxi_subconjunto(matriz, 3, 0, 0, []))
+print(calcular_maxi_subconjunto(matriz, 3))
---
+++ Guia 1/rutaMinima.py
@@ -0,0 +1,106 @@
+# Represento la funcion Pi de 1 a n -> 1 a n como un array de n elementos que la posicion seria el dominio y el valor el codominio
+
+min_actual = 10000
+llamadas_recu = 0
+# Con poda en caso base
+"""""
+def calcular_ruta_minima(matriz, funcion, i):
+    global min_actual
+    global llamadas_recu
+    if(i == len(funcion)):
+        return funcion
+    if(cantidad_vacios(funcion) + i > len(matriz)):
+        return []
+    # Poda caso base
+    suma_parcial_actual = calcular_suma_parcial(matriz, funcion)
+    if(suma_parcial_actual > min_actual):
+        return []
+    opciones_disponibles = calcular_opciones(funcion)
+    funcion_min = funcion[:]
+    for o in range(len(opciones_disponibles)):
+        if opciones_disponibles[o] == True:
+            agrego = funcion[:]
+            agrego[i] = o
+            llamadas_recu += 1
+            funcion_agrego = calcular_ruta_minima(matriz, agrego, i+1)
+            if(calcular_suma(matriz, funcion_agrego) < calcular_suma(matriz, funcion_min)): 
+                funcion_min = funcion_agrego
+                min_actual = calcular_suma(matriz, funcion_agrego)
+    return funcion_min
+"""
+
+# Con poda antes de agregar
+def calcular_ruta_minima(matriz, funcion, i):
+    global min_actual
+    global llamadas_recu
+    if(i == len(funcion)):
+        return funcion
+    if(cantidad_vacios(funcion) + i > len(matriz)):
+        return []
+    opciones_disponibles = calcular_opciones(funcion)
+    funcion_min = funcion[:]
+    for o in range(len(opciones_disponibles)):
+        if opciones_disponibles[o] == True:
+            agrego = funcion[:]
+            agrego[i] = o
+            # Poda en el cuerpo
+            suma_parcial_actual = calcular_suma_parcial(matriz, agrego)
+            if(suma_parcial_actual <= min_actual): 
+                llamadas_recu += 1
+                funcion_agrego = calcular_ruta_minima(matriz, agrego, i+1)
+                if(calcular_suma(matriz, funcion_agrego) < calcular_suma(matriz, funcion_min)): 
+                    funcion_min = funcion_agrego
+                    min_actual = calcular_suma(matriz, funcion_agrego)
+    return funcion_min           
+    
+
+def calcular_suma(matriz, funcion):
+    if(len(funcion) == 0 or cantidad_vacios(funcion) > 0):
+        return 100000
+    res = matriz[funcion[len(matriz) - 1]][funcion[0]]
+    for i in range(len(funcion) - 1):
+        res += matriz[funcion[i]][funcion[i + 1]]
+    return res
+
+def calcular_suma_parcial(matriz, funcion):
+    res = 0
+    if(funcion[len(matriz) - 1] != -1 and funcion[0] != -1):
+        res += matriz[funcion[len(matriz) - 1]][funcion[0]]
+    for i in range(len(funcion) - 1):
+        if(funcion[i] != -1 and funcion[i + 1] != -1):
+            res += matriz[funcion[i]][funcion[i + 1]]
+    if res == 0:
+        return 10000
+    else:
+        return res
+
+
+
+def calcular_opciones(funcion):
+    opciones = [True] * len(funcion)
+    for i in range(len(funcion)):
+        if(funcion[i] != -1):
+            opciones[funcion[i]] = False
+    return opciones
+
+def cantidad_vacios(funcion):
+    res = 0
+    for i in range(len(funcion)):
+        if(funcion[i] == -1):
+            res += 1
+    return res
+
+matriz = [
+    [0, 10, 15, 20],
+    [10, 0, 35, 25],
+    [15, 35, 0, 30],
+    [20, 25, 30, 0]
+]
+
+funcion = [-1,-1,-1,-1]
+
+
+ruta_minima = calcular_ruta_minima(matriz, funcion, 0)
+print(ruta_minima)
+print(llamadas_recu)
+print(calcular_suma(matriz, ruta_minima))
