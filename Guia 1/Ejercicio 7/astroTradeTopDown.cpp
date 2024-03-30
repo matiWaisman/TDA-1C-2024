3cc68a61c29f23fe00341487199a93cc0c12be92
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-03-30 13:52:53 -0300

    Added Ejercicio 7 top down

---
+++ Guia 1/Ejercicio 7/astroTradeTopDown.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> catalogo;
+vector<vector<int>> matriz;
+
+int maximo(int a, int b, int c) {
+    return std::max(std::max(a, b), c);
+}
+
+
+int av(int c, int j){
+    if(c < 0 || c > catalogo.size() - j){
+        return -10000;
+    }
+    if(j == catalogo.size()){
+        return 0;
+    }
+    if(matriz[c][j] == -10000){
+        if(c == 0){
+            int compro = av(c + 1, j + 1) - catalogo[j];
+            int noHagoNada = av(c, j + 1);
+            matriz[c][j] = max (compro, noHagoNada);
+        }
+        else if(c > 0){
+            int compro = av(c + 1, j + 1) - catalogo[j];
+            int vendo = av(c - 1, j + 1) + catalogo[j];
+            int noHagoNada = av(c, j + 1);
+            matriz[c][j] = maximo(compro, vendo, noHagoNada);
+        }
+    }
+    return matriz[c][j];
+}
+
+
+
+int main(){
+    catalogo = {6, 2, 5, 3};
+    matriz = vector<vector<int>>(catalogo.size(), vector<int>(catalogo.size(), -10000));
+    int res = av(0,0);
+    return res;
+}
