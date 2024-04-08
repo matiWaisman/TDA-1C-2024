c3d5a29ba3398c013eaf704dc26b66504da96367
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-04-08 14:13:52 -0300

    Prueba cambio manejo globales, sigue sin andar

--- Entregable 3/Acorn Matias Waisman.cpp
+++ Entregable 3/Acorn Matias Waisman.cpp
@@ -7,9 +7,6 @@ using namespace std;
 int t;
 int h;
 int f;
-vector<vector<int>> mem;
-vector<int> mejorFila;
-vector<vector<int>> squirrels;
 
 vector<vector<int>> palabras_a_numeros(vector<string> palabras) {
     vector<vector<int>> res(palabras.size());
@@ -30,19 +27,7 @@ vector<vector<int>> palabras_a_numeros(vector<string> palabras) {
     return res;
 }
 
-void vaciarMem(){
-    for(int i = 0; i < mem.size(); i++) {
-        mem[i].clear();
-    }
-}
-
-void vaciarSquirrels(){
-    for(int i = 0; i < squirrels.size(); i++){
-        squirrels[i].clear();
-    }
-}
-
-int acorn() {
+int acorn(vector<vector<int>> &mem, vector<int> &mejorFila ,vector<vector<int>> &squirrels) {
     for(int i = h - 1; i >= 0 && i > h - f - 1 ; i--) {
         for(int j = 0; j < t; j++) {
             i == h - 1 ? mem[i][j] = squirrels[j][i] : mem[i][j] = squirrels[j][i] + mem[i+1][j];
@@ -64,7 +49,9 @@ int main() {
     string linea;
     vector<string> palabras;
 
-    while(getline(cin, linea)) {
+    while(getline(cin, linea)) {vector<vector<int>> mem1(h,vector<int>(t, -1));
+        vector<int> mejorFila1 (h,0);
+        vector<vector<int>> squirrels1(t, vector<int>(h, 0));
         if (linea.empty()) {
             break;
         }
@@ -76,22 +63,16 @@ int main() {
         t = numeros[i][0];
         h = numeros[i][1];
         f = numeros[i][2];
-        mem.clear();
-        mejorFila.clear();
-        squirrels.clear();
-        mem.resize(h, vector<int>(t, -1));
-        mejorFila.resize(h,0);
-        squirrels.resize(t, vector<int>(h, 0));
-        
+        vector<vector<int>> mem(h,vector<int>(t, -1));
+        vector<int> mejorFila (h,0);
+        vector<vector<int>> squirrels(t, vector<int>(h, 0));
         for(int n = i + 1; n < i + t + 1; n++) {
             for(int j = 1; j < numeros[n].size(); j++) {
                 squirrels[n - i - 1][numeros[n][j]] += 1;
             }
         }
-        int res = acorn();
+        int res = acorn(mem, mejorFila, squirrels);
         cout << res << endl;
-        vaciarMem();
-        vaciarSquirrels();
         i = i + t + 1;
     }
     return 1;
