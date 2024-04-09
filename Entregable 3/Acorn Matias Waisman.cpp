169b2516af55538a7b87f59f4a96fe92ef54c7c1
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-04-09 12:14:50 -0300

    Terminado entregable 3

--- Entregable 3/Acorn Matias Waisman.cpp
+++ Entregable 3/Acorn Matias Waisman.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <queue>
 
 using namespace std;
 
-int t;
-int h;
-int f;
+int trees;
+int height;
+int fall;
+
+
+int acorn(vector<vector<int>> &mem, vector<vector<int>> &squirrels) {
+    queue<int> mejoreFilas;
+    for(int i = height - 1; i >= 0 && i > height - fall - 1 ; i--) {
+        int mejorFilaActual = -1;
+        for(int j = 0; j < trees; j++) {
+            i == height - 1 ? mem[i][j] = squirrels[j][i] : mem[i][j] = squirrels[j][i] + mem[i+1][j];
+            mejorFilaActual = max(mejorFilaActual, mem[i][j]);
+        }
+        mejoreFilas.push(mejorFilaActual);
+    }
+    for(int i = height - fall - 1; i >= 0; i--) {
+        int mejorBajada = mejoreFilas.front(); 
+        mejoreFilas.pop(); // Saco el elemento de la cola
+        int mejorFilaActual = -1;
+        for(int j = 0; j < trees; j++) {
+            int mismoArbol = mem[i + 1][j] + squirrels[j][i];
+            int otroArbol = mejorBajada + squirrels[j][i];
+            mem[i][j] = max(mismoArbol, otroArbol);
+            mejorFilaActual = max(mejorFilaActual, mem[i][j]);
+        }
+        mejoreFilas.push(mejorFilaActual);
+    }
+    return mejoreFilas.back();
+}
 
 vector<vector<int>> palabras_a_numeros(vector<string> palabras) {
     vector<vector<int>> res(palabras.size());
@@ -27,53 +54,37 @@ vector<vector<int>> palabras_a_numeros(vector<string> palabras) {
     return res;
 }
 
-int acorn(vector<vector<int>> &mem, vector<int> &mejorFila ,vector<vector<int>> &squirrels) {
-    for(int i = h - 1; i >= 0 && i > h - f - 1 ; i--) {
-        for(int j = 0; j < t; j++) {
-            i == h - 1 ? mem[i][j] = squirrels[j][i] : mem[i][j] = squirrels[j][i] + mem[i+1][j];
-            mejorFila[i] = max(mejorFila[i], mem[i][j]);
-        }
-    }
-    for(int i = h - f - 1; i >= 0; i--) {
-        for(int j = 0; j < t; j++) {
-            int mismoArbol = mem[i + 1][j] + squirrels[j][i];
-            int otroArbol = mejorFila[i + 2] + squirrels[j][i];
-            mem[i][j] = max(mismoArbol, otroArbol);
-            mejorFila[i] = max(mejorFila[i], mem[i][j]);
-        }
-    }
-    return mejorFila[0];
-}
-
 int main() {
     string linea;
     vector<string> palabras;
-
-    while(getline(cin, linea)) {vector<vector<int>> mem1(h,vector<int>(t, -1));
-        vector<int> mejorFila1 (h,0);
-        vector<vector<int>> squirrels1(t, vector<int>(h, 0));
+    int contadorInput = 0;
+    while(getline(cin, linea)) {
         if (linea.empty()) {
             break;
         }
         palabras.push_back(linea);
     }
     vector<vector<int>> numeros = palabras_a_numeros(palabras);
-    int i = 1;
-    while(i < numeros.size() - 1) {
-        t = numeros[i][0];
-        h = numeros[i][1];
-        f = numeros[i][2];
-        vector<vector<int>> mem(h,vector<int>(t, -1));
-        vector<int> mejorFila (h,0);
-        vector<vector<int>> squirrels(t, vector<int>(h, 0));
-        for(int n = i + 1; n < i + t + 1; n++) {
-            for(int j = 1; j < numeros[n].size(); j++) {
-                squirrels[n - i - 1][numeros[n][j]] += 1;
-            }
+    int cantidadTests = numeros[0][0];
+    int i = 1; // Representa el inicio de un test;
+    for(int test = 1; test <= cantidadTests; test++){
+        if(numeros[i][0] == 0){
+            i = i + 1;
         }
-        int res = acorn(mem, mejorFila, squirrels);
+        trees = numeros[i][0];
+        height = numeros[i][1];
+        fall = numeros[i][2];
+        int j = i + 1; // Posicion del primer arbol
+        vector<vector<int>> squirrels(trees, vector<int>(height, 0));
+        vector<vector<int>> mem(height, vector<int>(trees, 0));
+        for(int t = 0; t < trees; t++){
+            for(int c = 1; c < numeros[j].size(); c++){
+                squirrels[t][numeros[j][c] - 1] += 1;
+            }
+            j += 1;
+        }
+        int res = acorn(mem, squirrels);
         cout << res << endl;
-        i = i + t + 1;
+        i = j; 
     }
-    return 1;
 }
