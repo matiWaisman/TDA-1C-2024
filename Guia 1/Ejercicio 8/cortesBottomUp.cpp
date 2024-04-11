0720e0cacc1021150cb373de4123194f7de51bd5
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-04-11 10:38:26 -0300

    Avance ej 8 bu

--- Guia 1/Ejercicio 8/cortesBottomUp.cpp
+++ Guia 1/Ejercicio 8/cortesBottomUp.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -16,14 +17,26 @@ void llenarValores(vector<int> posCorte){
     }
 }
 
-//int armarSolucion(){
+int armarSolucion(vector<int> posCorte){
+    int longitud = valorCortes[0][valorCortes.size() - 1];
+    for(int d = 0; d < valorCortes.size(); d++){
+        for(int h = 1; h < valorCortes.size(); h++){
+            int longitudI = posCorte[h] - posCorte[d];
+            int longitudD = longitud - posCorte[h];
+            int costoI = 0;
+            int costoD = 0;
 
-//}
+        }
+    }
+    return 1;
+}
+
 
 int main(){
     int largo = 10;
     vector<int> posCorte = {0,2,4,7, 10};
     valorCortes = vector<vector<int>>(posCorte.size(), vector<int>(posCorte.size(), 0));
     llenarValores(posCorte);
+    int res = armarSolucion(posCorte);
     return 1;
 }
