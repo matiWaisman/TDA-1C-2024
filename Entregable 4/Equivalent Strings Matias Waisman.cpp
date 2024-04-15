295bcb6450f43e0cb5a829cb5ace78ad4ce49b9c
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-04-14 22:13:09 -0300

    Terminado taller 4

---
+++ Entregable 4/Equivalent Strings Matias Waisman.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string a;
+string b;
+
+string reverseString(const string& str) {
+    string reversed;
+    for (int i = str.length() - 1; i >= 0; --i) {
+        reversed.push_back(str[i]);
+    }
+    return reversed;
+}
+
+bool cumple(int iA, int fA, int iB, int fB){
+    int longitud = fA - iA;
+    if(longitud % 2 != 0){
+        for (int i = 0; i < longitud; ++i) {
+            if (a[iA + i] != b[iB + i])
+                return false;
+        }
+        return true;
+    }
+    if(longitud == 2){
+        string substrA = a.substr(iA, longitud);
+        string substrB = b.substr(iB, longitud);
+        return substrA == substrB || substrA == reverseString(substrB);
+    }
+    int mA = (iA + fA) / 2;
+    int mB = (iB + fB) / 2;
+    return ((cumple(iA, mA, mB, fB) && cumple(mA, fA, iB, mB)) || (cumple(iA, mA, iB, mB) && cumple(mA, fA, mB, fB)));
+}
+
+
+int main(){
+    cin >> a >> b;
+    int longitud = a.size();
+    if(a.size() > b.size() || a.size() < b.size()){
+        cout << "NO" << endl;
+    }
+    else{
+        bool res = cumple(0, longitud, 0, longitud); // Siempre j es excluyente y i incluye
+        if(res){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+   
+}
