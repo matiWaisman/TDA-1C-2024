e1c7e65895c256f0fec83d22ec6daac36ba3557e
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-04-17 14:56:52 -0300

    Mini avance

--- Guia 2/Ejercicio 9/mergeSelectivo.cpp
+++ Guia 2/Ejercicio 9/mergeSelectivo.cpp
@@ -5,8 +5,24 @@ using namespace std;
 vector<int> a; 
 vector<int> b;
 
+int dondeIria(int elem, vector<int> l){
+    int pivote = l.size() / 2;
+    bool encontre = false;
+    int res = -1000;
+    while(pivote > 0 && pivote < l.size() && encontre == false){
+        if(elem > l[pivote] && pivote == l.size() - 1){
+            encontre = true;
+        }
+    }
+}
+
+
 int iEsimoElemento(int i){
     int longitudTotal = a.size() + b.size();
+    int pivoteA = a.size() / 2;
+    int posicionActual = 
+    while()
+    return 4;
     
 }
 
@@ -14,5 +30,6 @@ int iEsimoElemento(int i){
 int main(){
     a = {1,3,5};
     b = {2,4,6};
+    iEsimoElemento(2);
     return 1;
 }
