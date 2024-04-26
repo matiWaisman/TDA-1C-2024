eae0c7cb08dde63e71da4fb40282216c5aff6f21
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-04-26 12:42:35 -0300

    Pase de heaps a vectores pero sigue rompiendose

--- Entregable 6/swap.cpp
+++ Entregable 6/swap.cpp
@@ -13,15 +13,15 @@ int res;
 
 void calcularCantidadSwaps(){
     res = 0;
-    priority_queue<int, vector<int>, greater<int>> necesitoA;
-    priority_queue<int, vector<int>, greater<int>> necesitoB;
+    vector<int> necesitoA;
+    vector<int> necesitoB;
     for(int i = 0; i < n; i++){
         if(s[i] != t[i]){
             if(t[i] == 'a'){
-                necesitoA.push(i);
+                necesitoA.push_back(i);
             }
             else if (t[i] == 'b'){
-                necesitoB.push(i);
+                necesitoB.push_back(i);
             }
         }
     }
@@ -29,32 +29,30 @@ void calcularCantidadSwaps(){
         res = - 1;
         return;
     }
-    while(necesitoB.size() > 1){
-        int swapS = necesitoB.top();
-        necesitoB.pop();
-        int swapT = necesitoB.top();
-        necesitoB.pop();
+    int bI = 0;
+    while(bI + 1 < necesitoB.size()){
+        int swapS = necesitoB[bI];
+        int swapT = necesitoB[bI + 1];
         char aux = s[swapS];
         s[swapS] = t[swapT];
         t[swapT] = aux;
         res += 1;
         swaps.push_back(pair(swapS + 1, swapT + 1));
+        bI += 2;
     }
-    if(necesitoB.size() == 1){
-        int posASwapear = necesitoB.top();
-        necesitoB.pop();
-        necesitoA.push(posASwapear);
+    if(bI == necesitoB.size() - 1){
+        int posASwapear = necesitoB[bI];
+        necesitoA.insert(necesitoA.begin(), posASwapear);
         char aux = s[posASwapear];
         s[posASwapear] = t[posASwapear];
         t[posASwapear] = aux;
         res += 1;
         swaps.push_back(pair(posASwapear + 1, posASwapear + 1));
+        bI += 1;
     }
-    while(necesitoA.size() > 1){
-        int swapS = necesitoA.top();
-        necesitoA.pop();
-        int swapT = necesitoA.top();
-        necesitoA.pop();
+    for(int aI = 0; aI + 1 < necesitoA.size(); aI += 2){
+        int swapS = necesitoA[aI];
+        int swapT = necesitoA[aI + 1];
         char aux = s[swapS];
         s[swapS] = t[swapT];
         t[swapT] = aux;
