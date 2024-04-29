c34b8cc4e2512dc04cba07eb73f67e31214a85b7
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-04-28 22:34:47 -0300

    Empece implementaciones guia 3

---
+++ Guia 3/Ejercicio 13/mellizos.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+vector<list<int>> listaDeAdyacencia;
+
+int encontrarMaximo(vector<pair<int, int>> aristas){
+    int res = -1;
+    for(int i = 0; i < aristas.size(); i++){
+        res = max(res,max(aristas[i].first, aristas[i].second));
+    }
+    return res;
+}
+
+void llenarLista(vector<pair<int, int>> aristas){
+    listaDeAdyacencia.resize(encontrarMaximo(aristas) + 1);
+    for(int i = 0; i < aristas.size(); i++){
+        listaDeAdyacencia[aristas[i].first].push_back(aristas[i].second);
+        listaDeAdyacencia[aristas[i].second].push_back(aristas[i].first);
+    }
+}
+
+
+
+
+int main(){
+    vector<pair<int, int>> aristas = {pair(1,2), pair(1,3), pair(1,4), pair(2,3), pair(3,4)};
+    llenarLista(aristas);
+    return 1;
+}
