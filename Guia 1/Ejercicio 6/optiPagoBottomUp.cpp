35a452e59f6a5e352ed9de895cad14b6b8697813
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-05-06 15:39:43 -0300

    Terminado bottom up ejercicio 6

--- Guia 1/Ejercicio 6/optiPagoBottomUp.cpp
+++ Guia 1/Ejercicio 6/optiPagoBottomUp.cpp
@@ -11,17 +11,38 @@ int j;
 // Primera posición es el exceso y la segunda la cantidad de billetes
 // Voy a guardar en la matriz en cada posicion, el exceso minimo y la cantidad minima de billetes para pagar un costo j con i cantidad de billetes
 
+pair<int,int> cualEsMejor(pair<int,int> a, pair<int,int> b){
+    if(a == b){
+        return min(a,b);
+    }
+    if(a.first < 0 && b.first < 0){
+        return max(a,b);
+    }
+    if(a.first <= 0 && b.first > 0){
+        return a;
+    }
+    if(a.first > 0 && b.first <= 0){
+        return b;
+    }
+    else{
+        return min(a,b);
+    }
+}
 
 pair<int,int> op(){
-    mem[0][0] = pair(0,0);
-    for(int i = 1; i < mem.size(); i++){
-        for(int j = mem[0].size() - 1; j >= 0; j--){
-            pair<int, int> costo = mem[i-1][max(0, j - conjunto[i])];
-            pair<int, int> nuevo_costo = make_pair(conjunto[i] + costo.first, 1 + costo.second);
-            mem[i][j] = min(nuevo_costo, mem[i-1][j]);
+    mem[mem.size() - 1][0] = pair(0,0);
+    for(int c = 1; c <= j; c++){
+        mem[mem.size() - 1][c] = make_pair(c - conjunto[mem.size() - 1], 1);
+    }
+    for(int f = mem.size() - 2; f >= 0; f--){
+        for(int c = 1; c <= j; c++){
+            pair<int, int> abajo = mem[f + 1][c];
+            pair<int,int> actual = make_pair(c - conjunto[f], 1);
+            pair<int,int> abajoMasActual = make_pair(abajo.first - conjunto[f], abajo.second + actual.second);
+            mem[f][c] = cualEsMejor(abajo, cualEsMejor(actual, abajoMasActual));
         }
     }
-    return mem[mem.size() - 1][mem[0].size() - 1];
+    return mem[0][j];
 }
 
 
@@ -29,7 +50,7 @@ pair<int,int> op(){
 int main() {
     conjunto = {1,3,5};
     j = 7;
-    mem = vector<vector<pair<int, int>>>(conjunto.size() + 1, vector<pair<int, int>>(j + 1, make_pair(1000,1000)));
+    mem = vector<vector<pair<int, int>>>(conjunto.size(), vector<pair<int, int>>(j + 1, make_pair(-1000,-1000)));
     pair<int,int> res = op();
     return 1;
 }
