1f9f128443e5e4c007dc7ca2083831455fc31658
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-05-07 17:24:17 -0300

    Update detectorCiclos.cpp

--- Guia 3/Ejercicio 14/detectorCiclos.cpp
+++ Guia 3/Ejercicio 14/detectorCiclos.cpp
@@ -6,7 +6,7 @@
 #include <unordered_set>
 
 using namespace std;
-
+using graph = vector<unordered_set<int>>;
 int encontrarMaximo(vector<pair<int, int>> aristas){
     int res = -1;
     for(int i = 0; i < aristas.size(); i++){
@@ -14,6 +14,8 @@ int encontrarMaximo(vector<pair<int, int>> aristas){
     }
     return res;
 }
+
+vector<unordered_set<int>> listaDeAdyacencia;
 
 vector<unordered_set<int>> llenarLista(vector<pair<int, int>> aristas){
     vector<unordered_set<int>> res = vector<unordered_set<int>>(encontrarMaximo(aristas) + 1);
@@ -58,33 +60,80 @@ vector<int> calcularDout(vector<pair<int, int>> aristas){
     return res;
 }
 
+/*
 vector<unordered_set<int>> eliminarGradoCero(vector<unordered_set<int>> listaDeAdyacencia){
     
     return res;
 }
+*/
 
 vector<bool> visitado;
+vector<int> camino;
+vector<int> sinGrado; 
 
-int cantidadDescendientes(int nodo, vector<unordered_set<int>> listaDeAdyacencia){
+pair<vector<int>,bool> cantidadDescendientes(int nodo,int padre){
     if(listaDeAdyacencia[nodo].size() == 0){
-        return 0;
+        if(padre != -1){
+            listaDeAdyacencia[padre].erase(nodo);
+        }
+        return {{nodo},false};
     }
     else{
-        int res = 0;
-        for(int hijo : listaDeAdyacencia[nodo]){
-            res += cantidadDescendientes(hijo, listaDeAdyacencia);
+        unordered_set<int> vecindario = listaDeAdyacencia[nodo];
+        for( int vecino : listaDeAdyacencia[nodo]){
+            pair<vector<int>,bool> res = cantidadDescendientes(vecino,nodo);
+            if( res.second == false){
+                sinGrado.(vecino);
+            }else{
+                camino.push_back(vecino);
+            }
         }
-        return res;
+        
+    }
+}
+
+vector<unordered_set<int>> destrurElgrafo(graph ADestruir){
+    bool habia0 = true;
+    while(habia0){
+        habia0 = false;
+        for( int nodo = 0;  nodo <ADestruir.size(); nodo++){
+            if(ADestruir[nodo].size() == 0){
+                habia = true;
+                destruirNodo(aDdestruir, nodo);
+            }
+        }
+    }
+}
+
+bool hayCiclo(int nodo, vector<bool> &visitados){
+    if(visitados[nodo]){
+        return true;
+    }
+    visitados[nodo] = true;
+    if(listaDeAdyacencia[nodo].size() == 0){
+        return false;
+    }
+    else{
+        bool res = false;
+        for(auto hijo : listaDeAdyacencia[nodo]){
+            if(hayCiclo(hijo,visitados)){
+                return true;
+            }
+        }
+        return false;
     }
 }
 
 
+
+
+
 int main(){
-    vector<pair<int, int>> aristasConCiclo = {pair(5,6),pair(5,4), pair(2,3), pair(3,5), pair(4,2), pair(1,2), pair(6,4)};
+    vector<pair<int, int>> aristasConCiclo = {pair(5,6),pair(5,4), pair(2,3), pair(3,5), pair(4,2), pair(1,2)};
     vector<pair<int, int>> aristasSinCiclo = {pair(2,4), pair(2,5), pair(1,3), pair(1,2)};
-    vector<unordered_set<int>> listaDeAdyacencia = llenarLista(aristasSinCiclo);
+    listaDeAdyacencia = llenarLista(aristasConCiclo);
     vector<int> gradoSalidaVertices = calcularDout(aristasSinCiclo);
     visitado = vector<bool>(listaDeAdyacencia.size(), false);
-    int des = cantidadDescendientes(1, listaDeAdyacencia);
+    int des = cantidadDescendientes(1, -1);
     return 1;
 }
