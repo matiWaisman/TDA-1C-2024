3a8cfbfdfa04be13cefb94f53504608bc26096cb
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-05-22 19:28:22 -0300

    Avances entregable 8, tira tle

--- Entregable 8/doveAndBombs.cpp
+++ Entregable 8/doveAndBombs.cpp
@@ -1,90 +1,121 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <list>
-#include <queue>
-#include <utility>
 
 using namespace std;
 
-vector<list<int>> listaDeAdyacencia;
-
-struct ComparePairs {
-    bool operator()(const pair<int, int>& p1, const pair<int, int>& p2) {
-        return p1.second < p2.second;
+void calcularExtremos(int vertice, int padre, vector<list<int>> &listaDeAdyacencia, vector<int> &padres, vector<int> &estado, vector<int> &extremoSuperiorEn, vector<int> &extremoInferiorEn, vector<list<int>> &treeEdges){
+    if(estado[vertice] == 0){
+        estado[vertice] = 1;
     }
-};
+    padres[vertice] = padre;
+    list<int> vecinos = listaDeAdyacencia[vertice];
+    for(auto vecino : vecinos){
+        if(estado[vecino] == 0){
+            treeEdges[vertice].push_back(vecino);
+            calcularExtremos(vecino, vertice, listaDeAdyacencia, padres, estado, extremoSuperiorEn, extremoInferiorEn, treeEdges);
+        }
+        else if(estado[vecino] == 1 && vecino != padre && padre != -1){
+            extremoInferiorEn[vertice] += 1;
+            extremoSuperiorEn[vecino] += 1;
+        }
+    }
+    estado[vertice] = 2;
+}
 
-vector<list<int>> llenarLista(vector<pair<int, int>> aristas, int n){
-    vector<list<int>> res = vector<list<int>>(n);
-    for(int i = 0; i < aristas.size(); i++){
-        res[aristas[i].first].push_back(aristas[i].second);
-        res[aristas[i].second].push_back(aristas[i].first);
+vector<int> mem;
+
+int cubren(int vertice, int padre, vector<int> extremoSuperiorEn, vector<int> extremoInferiorEn, vector<list<int>> treeEdges){
+    if(mem[vertice] != -1){
+        return mem[vertice];
+    }
+    int res = 0;
+    res += extremoInferiorEn[vertice];
+    res -= extremoSuperiorEn[vertice];
+    for(auto vecino : treeEdges[vertice]){
+        if(vecino != padre && vecino != -1){
+            res += cubren(vecino, vertice, extremoSuperiorEn, extremoInferiorEn, treeEdges);
+        }
+    }
+    mem[vertice] = res;
+    return res;
+}
+
+vector<pair<int,int>> tuplas;
+
+vector<pair<int,int>> merge(vector<pair<int,int>> a, vector<pair<int,int>> b){
+    int i = 0;
+    int j = 0;
+    vector<pair<int,int>> res; 
+    while(i < a.size() && j < b.size()){
+        if(a[i].second > b[j].second){
+            res.push_back(a[i]);
+            i++;
+        }
+        else{
+            res.push_back(b[j]);
+            j++;
+        }
+    }
+    while(i < a.size()){
+        res.push_back(a[i]);
+        i++;
+    }
+    while(j < b.size()){
+        res.push_back(b[j]);
+        j++;
     }
     return res;
 }
 
-void calcularTreeEdges(int vertice, int padre, vector<string> &estado, vector<list<int>> &treeEdges){
-    if(estado[vertice] == "sin_ver"){
-        estado[vertice] = "siendo_visto";
+vector<pair<int,int>> mergeSort(int i, int j){
+    int cantidadElementos = j - i;
+    if (cantidadElementos == 1) {
+        return vector<pair<int,int>>{tuplas[i]}; 
     }
-    list<int> vecinos = listaDeAdyacencia[vertice];
-    for(auto vecino : vecinos){
-        if(estado[vecino] == "sin_ver"){
-            treeEdges[vertice].push_back(vecino);
-            calcularTreeEdges(vecino, vertice, estado, treeEdges);
-        }
-    }
-    estado[vertice] = "visto";
+    int mitad = (i + j) / 2;
+    vector<pair<int,int>> mitadI = mergeSort(i, mitad);
+    vector<pair<int,int>> mitadD = mergeSort(mitad, j);
+    return merge(mitadI, mitadD);
 }
 
+// Codigo muy parecido al que vimos en clase con Martin porque sale con eso. 
 int main(){
-    std::ios_base::sync_with_stdio(false);
-    string linea;
-    vector<string> palabras;
-    int contadorInput = 0;
-    while(getline(cin, linea)) {
-        if (linea.empty()) {
-            break;
-        }
-        palabras.push_back(linea);
+    int n, m;
+    cin >> n >> m;
+    vector<list<int>> listaDeAdyacencia = vector<list<int>>(n, list<int>({}));
+    for(int i = 0; i < n; i++){
+        int v1, v2; 
+        cin >> v1 >> v2;
+        listaDeAdyacencia[v1].push_back(v2);
+        listaDeAdyacencia[v2].push_back(v1);
     }
-    int it = 0;
-    while(it < palabras.size()){
-        int n = stoi(string(1, palabras[it][0]));
-        int m = stoi(string(1, palabras[it][2]));
-        it += 1; 
-        vector<pair<int,int>> aristas;
-        while(palabras[it][0] != '-'){
-            pair<int,int> aristaActual = pair<int,int>(stoi(string(1, palabras[it][0])), stoi(string(1, palabras[it][2])));
-            aristas.push_back(aristaActual);
-            it++;
-        }
-        it += 2;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, ComparePairs> maxHeap;
-        vector<string> estado = vector<string>(n, "sin_ver");
-        vector<list<int>> treeEdges = vector<list<int>>(n);
-        listaDeAdyacencia = llenarLista(aristas, n);
-        for(int i = 0; i <= m; i++){
-            if(estado[i] == "sin_ver"){
-                calcularTreeEdges(i, -1, estado, treeEdges);
-            }
-        }
-        vector<int> cantidadPalomas = vector<int>(m + 1, 0);
-        for(int i = 0; i <= m; i++){
-            cantidadPalomas[i] = treeEdges[i].size();
-            if(treeEdges[i].size() != listaDeAdyacencia[i].size()){
-                cantidadPalomas[i] += 1;
-            }
-            maxHeap.push(pair<int, int>(i, cantidadPalomas[i]));
-        }
-        int i = 0;
-        while(i < m){
-            pair<int,int> tuplaActual = maxHeap.top();
-            maxHeap.pop();
-            cout << tuplaActual.first << " " << tuplaActual.second << endl;
-            i++;
+    vector<int> estado = vector<int>(n, 0); // 0 es sin ver, 1 es siendo visto y 2 es visto
+    vector<int> extremoSuperiorEn = vector<int>(n, 0);
+    vector<int> extremoInferiorEn = vector<int>(n, 0);
+    vector<list<int>> treeEdges = vector<list<int>>(n, list<int>({}));
+    vector<int> padres = vector<int>(n, -1);
+    for(int i = 0; i < n; i++){
+        if(estado[i] == 0){
+            calcularExtremos(i, -1, listaDeAdyacencia, padres, estado, extremoSuperiorEn, extremoInferiorEn, treeEdges);
         }
     }
-    return 1;
+    mem = vector<int>(n, -1);
+    for(int i = 0; i < n; i++){
+        int cubrenActual = cubren(i, -1, extremoSuperiorEn, extremoInferiorEn, treeEdges);
+        if(cubrenActual == 0){
+            if(padres[i] != -1){
+                tuplas.emplace_back(pair<int, int>(i,treeEdges[i].size() + 1));
+            }
+            else{
+                tuplas.emplace_back(pair<int, int>(i,treeEdges[i].size()));
+            }
+        }
+    }
+    vector<pair<int,int>> res = mergeSort(0, tuplas.size());
+    for(int i = 0; i < m; i++){
+        pair<int,int> tuplaActual = tuplas[i];
+        cout << tuplaActual.first << " " << tuplaActual.second << endl;
+    } 
+    tuplas.clear();
 }
