2668a2079a4b90b7f2b853fbb6f21403b053f614
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-05-27 15:36:18 -0300

    Terminado entregable 8

--- Entregable 8/Doves Matias Waisman.cpp
+++ Entregable 8/Doves Matias Waisman.cpp
@@ -8,32 +8,34 @@ using namespace std;
 
 int t;
 
-void calcularPuentes(int vertice, vector<list<int>> listaDeAdyacencia, vector<int> &padres, vector<int> &tiempos, vector<string> &estados, vector<int> &menor,vector<int> &cantidadTreeedges, unordered_set<int> &aps){
+void calcularPuentes(int vertice, vector<list<int>> &listaDeAdyacencia, vector<int> &padres, vector<int> &tiempos, vector<string> &estados, vector<int> &menor,vector<list<int>> &treeEdges, unordered_set<int> &aps){
     estados[vertice] = "Siendo visitado";
     list<int> vecinos = listaDeAdyacencia[vertice];
     menor[vertice] = t;
     tiempos[vertice] = t;
     t += 1;
     int contadorHijos = 0;
+    int menorAux = menor[vertice];
     for(auto vecino : vecinos){
         if(vecino != padres[vertice]){
             if(estados[vecino] == "Sin visitar"){
-                cantidadTreeedges[vertice] += 1;
                 contadorHijos += 1;
                 padres[vecino] = vertice;
-                calcularPuentes(vecino, listaDeAdyacencia, padres, tiempos, estados, menor, cantidadTreeedges, aps);
-                if(tiempos[vertice] <= menor[vecino]){
+                calcularPuentes(vecino, listaDeAdyacencia, padres, tiempos, estados, menor, treeEdges, aps);
+                if(tiempos[vertice] <= menor[vecino] && listaDeAdyacencia[vertice].size() > 1){
                     aps.insert(vertice);
+                    treeEdges[vertice].push_back(vecino);
                 } 
                 else{
-                    menor[vertice] = min(menor[vertice], menor[vecino]);
+                    menorAux = min(menorAux, menor[vecino]);
                 }
             }   
             else{
-                menor[vertice] = min(menor[vertice], menor[vecino]);
+                menorAux = min(menorAux, menor[vecino]);
             }
         }     
-    }
+    } 
+    menor[vertice] = menorAux;
     if(padres[vertice] == -1 && contadorHijos >= 2){
         aps.insert(vertice);
     }
@@ -41,48 +43,57 @@ void calcularPuentes(int vertice, vector<list<int>> listaDeAdyacencia, vector<in
 }
 
 bool compararPorSegundo(const pair<int, int> &a, const pair<int, int> &b) {
-    return a.second > b.second;
+    if(a.second != b.second){
+        return a.second > b.second;
+    }
+    else{
+        return a.first < b.first;
+    }
+    
 }
 
 int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int n, m;
     cin >> n >> m;
     while(n != 0 && m != 0){
         int x, y;
-        vector<list<int>> listaDeAdyacencia = vector<list<int>>(n, list<int>{});
+        vector<list<int>> listaDeAdyacencia(n);
         cin >> x >> y;
         while(x != -1 && y != -1){
             listaDeAdyacencia[x].push_back(y);
             listaDeAdyacencia[y].push_back(x);
             cin >> x >> y;
         }
-        vector<int> padres = vector<int>(n, -1);
-        vector<int> tiempos = vector<int>(n,1000);
-        vector<string> estados = vector<string>(n, "Sin visitar");
-        vector<int> menor = vector<int>(n,1000);
-        vector<int> cantidadTreedges = vector<int>(n, 0);
+        vector<int> padres(n, -1);
+        vector<int> tiempos(n,1000);
+        vector<string> estados(n, "Sin visitar");
+        vector<int> menor(n,1000);
+        vector<list<int>> treeEdges(n);
         unordered_set<int> aps;
         t = 0;
         for(int i = 0; i < n; i++){
             if(estados[i] == "Sin visitar"){
-                calcularPuentes(i, listaDeAdyacencia, padres, tiempos, estados, menor, cantidadTreedges, aps);
+                calcularPuentes(i, listaDeAdyacencia, padres, tiempos, estados, menor, treeEdges, aps);
             }
         }
         vector<pair<int,int>> res;
+        for(int i = 0; i < n; i++){
+            res.push_back(pair(i, 1));
+        }
         for(auto ap: aps){
             if(padres[ap] != -1){
-                res.push_back(pair(ap, cantidadTreedges[ap] + 1));
+                res[ap].second = treeEdges[ap].size() + 1;
             }
             else{
-                res.push_back(pair(ap, cantidadTreedges[ap]));
+                res[ap].second = treeEdges[ap].size();
             }
         }
         sort(res.begin(), res.end(), compararPorSegundo);
-        for(int i = 0; i < m && i < res.size(); i++){
+        for(int i = 0; i < m; i++){
             cout << res[i].first << " " << res[i].second << endl;
         }
         cout << "\n";
         cin >> n >> m;
     }
-    return 1;
 }
