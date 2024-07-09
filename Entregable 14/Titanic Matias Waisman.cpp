9bcc044dfbe43c9a0f4d86c970924a75d91e1a5f
matiWaisman <83453393+matiWaisman@users.noreply.github.com>
2024-07-09 13:57:32 -0300

    Added entregable 14

--- Entregable 14/Titanic Matias Waisman.cpp
+++ Entregable 14/Titanic Matias Waisman.cpp
@@ -3,24 +3,13 @@
 #include <list>
 #include <vector>
 #include <queue>
-#include <unordered_map>
 #include <utility>
 
 using namespace std;
 
 const int inf = 1e9;
 
-// https://stackoverflow.com/questions/32685540/why-cant-i-compile-an-unordered-map-with-a-pair-as-key
-struct pairHash {
-    template <class T1, class T2>
-    size_t operator () (const pair<T1,T2> &p) const {
-        auto h1 = hash<T1>{}(p.first);
-        auto h2 = hash<T2>{}(p.second);
-        return h1 ^ h2;  
-    }
-};
-
-bool bfs(vector<vector<int>> matrizResidual, int fuente, int sumidero, vector<int> &padres){
+bool bfs(vector<vector<int>> &matrizResidual, int fuente, int sumidero, vector<int> &padres){
     vector<bool> visitados(matrizResidual.size(), false);
     queue<int> q;
     q.push(fuente);
@@ -43,7 +32,7 @@ bool bfs(vector<vector<int>> matrizResidual, int fuente, int sumidero, vector<in
     return false;
 }
 
-int encontrarCuelloDeBotella(vector<vector<int>> matrizResidual, int fuente, int sumidero, vector<int> padres){
+int encontrarCuelloDeBotella(vector<vector<int>> &matrizResidual, int fuente, int sumidero, vector<int> &padres){
     int res = inf; 
     int actual = sumidero;
     while(actual != fuente){
@@ -54,7 +43,7 @@ int encontrarCuelloDeBotella(vector<vector<int>> matrizResidual, int fuente, int
     return res;
 }
 
-int fordFulkerson(vector<vector<int>> matrizCapacidades, int fuente, int sumidero){
+int fordFulkerson(vector<vector<int>> &matrizCapacidades, int fuente, int sumidero){
     vector<vector<int>> matrizResidual = matrizCapacidades;
     vector<int> padres(matrizCapacidades.size(), -1);
     int res = 0;
@@ -86,7 +75,7 @@ vector<pair<int, int>> calcularPosicionesAdyacentes(pair<int, int> posicion, int
         int nuevaFila = posicion.first + movimiento.first;
         int nuevaColumna = posicion.second + movimiento.second;
 
-        // Verificar si la nueva posición está dentro de los límites del tablero
+        // Verificar si la nueva posición esta dentro de los limites del tablero
         if (nuevaFila >= 0 && nuevaFila < x && nuevaColumna >= 0 && nuevaColumna < y) {
             adyacentes.push_back({nuevaFila, nuevaColumna});
         }
@@ -94,39 +83,39 @@ vector<pair<int, int>> calcularPosicionesAdyacentes(pair<int, int> posicion, int
     return adyacentes;
 }
 
-void conectarMovimientos(pair<int,int> posicion, vector<pair<int,int>> adyacentes, vector<vector<string>> &matrizOriginal, vector<vector<int>> &matrizDeAdyacencia, unordered_map<pair<int,int>, int, pairHash> &posicionesMap, unordered_map<int, int> &puntosMap){
+void conectarMovimientos(pair<int,int> posicion, vector<pair<int,int>> adyacentes, vector<vector<char>> &matrizOriginal, vector<vector<int>> &matrizDeAdyacencia, int puntosVistos, int columnas, int verticesMinimos){
     int x = posicion.first;
     int y = posicion.second;
-    int verticeActual = posicionesMap.at(posicion);
-    if(matrizOriginal[x][y] == "*"){
+    int verticeActual = x * columnas + y + 1;
+    if(matrizOriginal[x][y] == '*'){
         matrizDeAdyacencia[0][verticeActual] = 1;
         for(auto movimiento: adyacentes){
             int i = movimiento.first;
             int j = movimiento.second;
-            int movimientoActual = posicionesMap.at(movimiento);
-            if(matrizOriginal[i][j] != "~" && matrizOriginal[i][j] != "*"){
+            int movimientoActual = i * columnas + j + 1;
+            if(matrizOriginal[i][j] != '~' && matrizOriginal[i][j] != '*'){
                 matrizDeAdyacencia[verticeActual][movimientoActual] = 1;
             }
         }
     }
-    if(matrizOriginal[x][y] == "."){
-        int splitter = puntosMap.at(verticeActual);
+    if(matrizOriginal[x][y] == '.'){
+        int splitter = verticesMinimos + puntosVistos;
         matrizDeAdyacencia[verticeActual][splitter] = 1;
         for(auto movimiento: adyacentes){
             int i = movimiento.first;
             int j = movimiento.second;
-            int movimientoActual = posicionesMap.at(movimiento);
-            if(matrizOriginal[i][j] != "~" && matrizOriginal[i][j] != "*"){
+            int movimientoActual = i * columnas + j + 1;
+            if(matrizOriginal[i][j] != '~' && matrizOriginal[i][j] != '*'){
                 matrizDeAdyacencia[splitter][movimientoActual] = 1;
             }
         }
     }
-    if(matrizOriginal[x][y] == "@"){
+    if(matrizOriginal[x][y] == '@' || matrizOriginal[x][y] == '#'){
         for(auto movimiento: adyacentes){
             int i = movimiento.first;
             int j = movimiento.second;
-            int movimientoActual = posicionesMap.at(movimiento);
-            if(matrizOriginal[i][j] != "~" && matrizOriginal[i][j] != "*"){
+            int movimientoActual = i * columnas + j + 1;
+            if(matrizOriginal[i][j] != '~' && matrizOriginal[i][j] != '*'){
                 matrizDeAdyacencia[verticeActual][movimientoActual] = inf;
             }
         }
@@ -137,43 +126,42 @@ void conectarMovimientos(pair<int,int> posicion, vector<pair<int,int>> adyacente
 int main(){
     int x, y, p;
     while(cin >> x >> y >> p){
-        vector<vector<string>> matrizOriginal(x, vector<string>(y, ""));
+        vector<vector<char>> matrizOriginal(x, vector<char>(y));
         int contadorPuntos = 0; 
         int verticesTotalesMinimos = x * y + 1;
-        int contadorVertices = 1;
-        unordered_map<pair<int,int>, int, pairHash> posicionesMap;
-        unordered_map<int, int> puntosMap; // Va a matchear a un punto con su bifurcador
         // Este primer ciclo lo hago para leer el input y para determinar la cantidad de puntos que hay. Que determinan la cantidad de vertices porque por cada uno de ellos voy a poner un vertice extra
         for(int i = 0; i < x; i++){
             for(int j = 0; j < y; j++){
-                char c; 
-                cin >> c; 
-                matrizOriginal[i][j] = c;
-                posicionesMap[{i,j}] = contadorVertices;
-                if(c == '.'){
+                cin >> matrizOriginal[i][j];
+                if(matrizOriginal[i][j] == '.'){
                     contadorPuntos += 1;
-                    puntosMap[contadorVertices] = contadorPuntos + verticesTotalesMinimos;
                 }
-                contadorVertices += 1;
             }
         }
         // El primer vertice es la fuente, despues vienen las posiciones originales, despues los splitters de puntos y por ultimo el sumidero.
         int verticesTotales = 2 + x * y + contadorPuntos;
         int sumidero = verticesTotales - 1;
         vector<vector<int>> matrizDeAdyacencia(verticesTotales, vector<int>(verticesTotales, 0));
-        for(int i = 0; i < y; i++){
-            for(int j = 0; j < x; j++){
-                int vertice = posicionesMap.at(pair(i,j));
-                if(matrizOriginal[i][j] == "#"){
+        int puntosVistos = -1;
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                int vertice = i * y + j + 1;
+                if(matrizOriginal[i][j] == '#'){
                     matrizDeAdyacencia[vertice][sumidero] = p;
+                    vector<pair<int,int>> vecinos = calcularPosicionesAdyacentes({i,j}, x, y);
+                    conectarMovimientos({i,j}, vecinos, matrizOriginal, matrizDeAdyacencia, puntosVistos, y, verticesTotalesMinimos);
                 }
-                else if(matrizOriginal[i][j] != "~"){
-                    vector<pair<int,int>> vecinos = calcularPosicionesAdyacentes(pair(i,j), x, y);
-                    conectarMovimientos(pair(i,j), vecinos, matrizOriginal, matrizDeAdyacencia, posicionesMap, puntosMap);
+                else if(matrizOriginal[i][j] != '~'){
+                    if(matrizOriginal[i][j] == '.'){
+                        puntosVistos ++;
+                    }
+                    vector<pair<int,int>> vecinos = calcularPosicionesAdyacentes({i,j}, x, y);
+                    conectarMovimientos({i,j}, vecinos, matrizOriginal, matrizDeAdyacencia, puntosVistos, y, verticesTotalesMinimos);
                 }
             }
         }
         int res = fordFulkerson(matrizDeAdyacencia, 0, sumidero);
         cout << res << '\n';
+        // Felices vacaciones!!!
     }
 }
